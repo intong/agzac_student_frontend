8a9afc0bfbbfc3ecf44ee105dcac1442dbdb1bf0
intong <intong@kakao.com>
2021-04-25 18:14:05 +0900

    2021-04-25 * 25일 배포용

--- src/api/api.js
+++ src/api/api.js
@@ -1,19 +1,18 @@
 import axios from "axios";
 
 // 배포용
-// const api = axios.create({ baseURL: "http://13.125.210.253:3000" });
-
-// const apiToken = axios.create({
-// 	baseURL: "http://13.125.210.253:3000",
-// 	headers: { token: sessionStorage.getItem("auth") },
-// });
-
-// 개발용
-const api = axios.create({});
+const api = axios.create({ baseURL: "http://13.125.210.253:3000" });
 
 const apiToken = axios.create({
+	baseURL: "http://13.125.210.253:3000",
 	headers: { token: sessionStorage.getItem("auth") },
 });
+
+// 개발용
+// const api = axios.create({});
+// const apiToken = axios.create({
+// 	headers: { token: sessionStorage.getItem("auth") },
+// });
 
 export const Student = {
 	loginStudent: (
